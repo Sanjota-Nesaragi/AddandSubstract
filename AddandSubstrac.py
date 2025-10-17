6267fcfdccee405925a13e134c42843bff5e1134
Sanjota.Nesaragi <01fe24bca239@kletech.ac.in>
2025-10-17 09:53:07 +0530

    Addition and Substraction of Two Numbers.

---
+++ AddandSubstrac.py
@@ -0,0 +1,14 @@
+# Program to perform addition and subtraction
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# Main Program
+num1 = 10
+num2 = 5
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
